420443f84fd6708753708af0c6b393cd38a10eb1
18016638 <18016638@myrp.edu.sg>
2019-07-05 16:24:53 +0800

    submission

--- app/src/main/java/sg/edu/rp/soi/c346_p08quiz/MainActivity.java
+++ app/src/main/java/sg/edu/rp/soi/c346_p08quiz/MainActivity.java
@@ -2,12 +2,44 @@ package sg.edu.rp.soi.c346_p08quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
+    ArrayAdapter<CharSequence>adapter;
+    Button btnsave;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner=(Spinner)findViewById(R.id.spinner);
+        adapter=ArrayAdapter.createFromResource(this,R.array.Class_names,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(),parent.getItemIdAtPosition(position)+" is selected",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+
 }
+
